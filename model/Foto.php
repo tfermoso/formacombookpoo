b90afbe354b4f99179c48d7a7803f6f8a84fa3ee
tfermoso <tomas.fermoso@gmail.com>
2025-12-17 19:19:15 +0100

    creando las clases necesarias

---
+++ model/Foto.php
@@ -0,0 +1,30 @@
+<?php
+class Foto{
+    private $fotos_id;
+    private $titulo;
+    private $descripcion;
+    private $ruta;
+    private $votos;
+
+    public function __construct($fotos_id=null, $titulo, $descripcion, $ruta){
+        $this->fotos_id = $fotos_id;
+        $this->titulo = $titulo;
+        $this->descripcion = $descripcion;
+        $this->ruta = $ruta;
+        $this->votos = array();
+    }
+    // Getters
+    public function fotos_id(){
+        return $this->fotos_id;
+    }       
+    public function titulo(){
+        return $this->titulo;
+    }
+    public function descripcion(){
+        return $this->descripcion;
+    }
+    public function ruta(){
+        return $this->ruta;
+    }
+}
+
---
+++ utils/Database.php
@@ -0,0 +1,42 @@
+<?php 
+<?php
+
+class Database {
+
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+    private $conn;
+
+    public function __construct() {
+        require_once __DIR__ . '/../config.php';
+        $this->host = DB_HOST;
+        $this->db_name = DB_NAME;
+        $this->username = DB_USER;
+        $this->password = DB_PASS;
+    }   
+
+    // Método para obtener la conexión
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8",
+                $this->username,
+                $this->password
+            );
+
+            // Configuración recomendada
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $exception) {
+            die("Error de conexión: " . $exception->getMessage());
+        }
+
+        return $this->conn;
+    }
+}
+
+?>
