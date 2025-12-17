e93b37f1a91539e7fce87a2549c78ff18b6d7e69
tfermoso <tomas.fermoso@gmail.com>
2025-12-17 19:43:26 +0100

    creando array de fotos

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-require_once 'model/Foto.php';
+require_once 'utils/Database.php';
+
+
+$db=new Database();
+$conn=$db->getConnection();
+
+$stmt=$conn->prepare('select * from fotos');
+$stmt->execute();
+$result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+$fotos=array();
+foreach($result as $row){
+    require_once 'model/Foto.php';
+    $foto=new Foto(
+        $row['fotos_id'],
+        $row['titulo'],
+        $row['descripcion'],
+        $row['ruta']
+    );
+  
+    array_push($fotos,$foto);
+}
 
